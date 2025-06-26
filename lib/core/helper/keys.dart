8a2ea902340bfb71ffbdc3d51c15e8f04da99032
Mohamed Madkour <madkourmohamed88@gmail.com>
2025-06-26 15:25:04 +0300

    Stop tracking keys.dart

--- lib/core/helper/keys.dart
+++
@@ -1,4 +0,0 @@
-const kClientId =
-    'AWIrJ0FAVC7_0vjJEwh6_xWqYrDo8-vVA4FljsjgDY1seIGCKzyVmk0O6xQHKX17E10YHtvXENG2-g_4';
-const kSecretId =
-    'EEQpxhkCu6ca3h6Q4tX1QgScQ28USDBx7AZ4xadz1FNvAm0J1Qgi-DvTa_u8QVGIHSAB_0Ql966U9HnF';
